5625ae3c74ae26242e44cec1620532c4d1f2e230
mnuralim <shadownur345.com>
2023-04-13 10:13:51 +0800

    update

---
+++ app/products/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <div className=" loader border-4 border-gray-300 rounded-full w-16 h-16"></div>
+    </div>
+  );
+};
+
+export default Loading;
