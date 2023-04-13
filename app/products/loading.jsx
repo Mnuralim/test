7ebcd651140a46b3ea047e3f97ab619a5240c4af
mnuralim <shadownur345.com>
2023-04-13 11:25:36 +0800

    update

--- app/products/loading.jsx
+++ app/products/loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen ">
+    <div className="flex justify-center items-center h-screen mb-28">
       <div className=" loader border-4 border-gray-300 rounded-full w-16 h-16"></div>
     </div>
   );
